412ed099e95abcefa2f09cfee9cf4043e3db2411
Guilherme Sousa <142254512+guilhermesous@users.noreply.github.com>
2023-11-04 14:51:14 -0300

    Create Mercado.sql
    Banco de Dados

---
+++ database/Mercado.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Mercado;
+
+USE Mercado;
+
+CREATE TABLE Produto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidade INT NOT NULL
+);
+
+CREATE TABLE Venda (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    quantidade INT NOT NULL,
+    idProduto INT,
+    FOREIGN KEY (idProduto) REFERENCES Produto(id)
+);
